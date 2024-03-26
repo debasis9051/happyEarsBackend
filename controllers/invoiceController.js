6618bfde91f68d85997de0929e9e27b4b66f023e
Hritwick De <hritwick14@gmail.com>
2024-03-26 09:27:39 +0530

    invoice product validation

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -35,6 +35,11 @@ const invoiceController = {
                 return res.status(200).json({ operation: "failed", message: "Invoice against given Invoice Number already exists" });
             }
 
+            let t2 = await Product.are_serials_in_stock(req.body.line_items.map(x => x.serial_number))
+            if (t2.find(x=>!x.instock)) {
+                return res.status(200).json({ operation: "failed", message: `These serials are not in stock currently: ${t2.filter(x=>!x.instock).map(x=>x.serial_number).join(", ")}` });
+            }
+
             await Invoice.add_invoice(req.body.current_user_uid, req.body.current_user_name, req.body)
 
             await Product.invoice_batch_products_with_logs(req.body.line_items, req.body.current_user_uid, req.body.current_user_name, "invoiced", "product invoiced", req.body.branch_id)
