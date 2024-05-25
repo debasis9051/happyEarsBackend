9a1460f769f6c68f18c5b58768667152413272e2
Hritwick De <hritwick14@gmail.com>
2024-05-25 12:59:31 +0530

    audiometry form update complete

--- models/audiometryModel.js
+++ models/audiometryModel.js
@@ -7,7 +7,7 @@ class Audiometry {
 
         let q = admin.firestore().collection('audiometry').orderBy("patient_name")
         let qs = await q.get()
-        return qs.docs.map(doc => ({id: doc.id, ...(doc.data())}))
+        return qs.docs.map(doc => ({ id: doc.id, ...(doc.data()) }))
     }
 
     static async get_audiometry_report_by_audiometry_report_id(audiometry_report_id) {
@@ -18,19 +18,40 @@ class Audiometry {
         return doc.data()
     }
 
-    static async add_audiometry_report(current_user_uid,current_user_name,body_data) {
+    static async add_audiometry_report(current_user_uid, current_user_name, body_data) {
         console.log('adding audiometry report')
 
         let audiometry_report_ref = await admin.firestore().collection('audiometry').add({
+            trial_mode: body_data.trial_mode,
+
             patient_name: body_data.patient_name,
-            patient_address: body_data.patient_address,
             contact_number: body_data.contact_number,
             age: body_data.age,
             sex: body_data.sex,
-            test_machine: body_data.test_machine,
+            patient_address: body_data.patient_address,
+
+            recommended_machine: body_data.recommended_machine,
+            client_chosen_machine: body_data.client_chosen_machine,
             remarks: body_data.remarks,
-            left_ear_pta: body_data.left_ear_pta,
-            right_ear_pta: body_data.right_ear_pta,
+
+            referred_by: body_data.referred_by,
+            audiometer: body_data.audiometer,
+            complaint: body_data.complaint,
+
+            ac_left_ear_pta: body_data.ac_left_ear_pta,
+            ac_right_ear_pta: body_data.ac_right_ear_pta,
+
+            bc_input: body_data.bc_input,
+            bc_left_ear_pta: body_data.bc_left_ear_pta,
+            bc_right_ear_pta: body_data.bc_right_ear_pta,
+
+            tuning_fork: body_data.tuning_fork,
+            rinne: body_data.rinne,
+            weber: body_data.weber,
+            doctor_id: body_data.doctor_id,
+
+            provisional_diagnosis: body_data.provisional_diagnosis,
+            recommendations: body_data.recommendations,
 
             created_at: admin.firestore.FieldValue.serverTimestamp(),
             added_by_user_uid: current_user_uid,
@@ -45,14 +66,32 @@ class Audiometry {
 
         await admin.firestore().collection('audiometry').doc(body_data.audiometry_report_id).update({
             patient_name: body_data.patient_name,
-            patient_address: body_data.patient_address,
             contact_number: body_data.contact_number,
             age: body_data.age,
             sex: body_data.sex,
-            test_machine: body_data.test_machine,
+            patient_address: body_data.patient_address,
+
+            recommended_machine: body_data.recommended_machine,
+            client_chosen_machine: body_data.client_chosen_machine,
             remarks: body_data.remarks,
-            left_ear_pta: body_data.left_ear_pta,
-            right_ear_pta: body_data.right_ear_pta,
+
+            referred_by: body_data.referred_by,
+            audiometer: body_data.audiometer,
+            complaint: body_data.complaint,
+
+            ac_left_ear_pta: body_data.ac_left_ear_pta,
+            ac_right_ear_pta: body_data.ac_right_ear_pta,
+
+            bc_input: body_data.bc_input,
+            bc_left_ear_pta: body_data.bc_left_ear_pta,
+            bc_right_ear_pta: body_data.bc_right_ear_pta,
+
+            tuning_fork: body_data.tuning_fork,
+            rinne: body_data.rinne,
+            weber: body_data.weber,
+
+            provisional_diagnosis: body_data.provisional_diagnosis,
+            recommendations: body_data.recommendations,
         });
     }
 }
